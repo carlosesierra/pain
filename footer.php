78ff018c17903298794cc77b12a51a94f464a780
Carlos Sierra <carlosesierra@gmail.com>
2017-10-15 22:17:38 +1100

    footer and css
    name of site in footer width of content max 1200px

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-<?php echo site_url(); ?>  &#169; <?php echo date("Y"); ?> - <a href="http://www.carlosesierra.com.au" target="blank_">web design</a> by carlosesierra.com.au.
+<?php echo get_bloginfo( $show = 'name' ); ?> &#169; <?php echo date("Y"); ?> - <a href="http://www.carlosesierra.com.au" target="blank_">web design</a> by carlosesierra.com.au.
 </div>
 <?php wp_footer(); ?>
 </body>
